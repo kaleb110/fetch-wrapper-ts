13cf657cdff5a770dd79c1a6bb9272fe22fe73c0
kaleb110 <kalisha123k@gmail.com>
2025-03-02 18:39:49 +0300

    fix: update export statement for foxios in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 
 
-export * from './core/foxios'; // Example: Export types from the core
+export { default as foxios } from './core/foxios'; // Example: Export types from the core
 export * from './errors/foxiosError'; // Export types from errors
 export * from './utils/buildQueryParams'; // Export types from utils
 export * from './types/index'; // Export types from types
